af3aed892e3df023ba9a7f04ebabc3c4c65d8562
Saad Quadri <saadquad@gmail.com>
2021-12-03 12:16:09 -0500

    Add docs and rename denotex => latrex

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# denotex
+# latrex
 
 > A small wrapper for generating PDFs with LaTeX in Deno.
 
@@ -12,17 +12,17 @@ LaTeX must be installed on your machine. You can download it [here](https://www.
 ### TeX file
 
 ```ts
-import { denotex } from "https://deno.land/x/denotex/mod.ts";
+import { latrex } from "https://deno.land/x/latrex/mod.ts";
 
 const document = await Deno.readFile("./document.tex");
-const pdf = await denotex(document);
+const pdf = await latrex(document);
 await Deno.writeFile("./output.pdf", pdf);
 ```
 
 ### TeX string
 
 ```ts
-import { denotex } from "https://deno.land/x/denotex/mod.ts";
+import { latrex } from "https://deno.land/x/latrex/mod.ts";
 
 const document = `
   \\documentclass{article}
@@ -31,14 +31,14 @@ const document = `
   \\end{document}
 `
 
-const pdf = await denotex(document);
+const pdf = await latrex(document);
 await Deno.writeFile("./output.pdf", pdf);
 ```
 
-### Options
+### With all options
 
 ```ts
-const pdf = await denotex(document, { 
+const pdf = await latrex(document, { 
   command: 'xelatex',
   args: ['-no-file-line-error', '-no-pdf'],
   inputs: ['./styles', './fonts'],
@@ -46,3 +46,13 @@ const pdf = await denotex(document, {
   errorLogsPath: './my-tex-errors.log'
 });
 ```
+
+### Options
+
+| **Option**      | **Type**   | **Default**          | **Description**                                                                                                               | 
+| --------------- | ---------- | -------------------- | ----------------------------------------------------------------------------------------------------------------------------- |
+| `command`       | `string`   | `'pdflatex'`         | The command to run for your compiling your LaTeX document (pdflatex, xetex, /user/bin/custom-tex, etc).                       |
+| `args`          | `string[]` | `['-halt-on-error']` | Arguments passed to the command.                                                                                              |
+| `passes`        | `number`   | `1`                  | The number of times to run `options.command`. Some documents require multiple passes. Only works when doc is a string.        |
+| `inputs`        | `string[]` | N/A                  | A list of absolute paths to the directory which contains the assets necessary for the doc (such as fonts or cls files, etc).  |
+| `errorLogsPath` | `string`   | N/A                  | The path to the file where you want to save the contents of the error log to.                                                 |
--- mod.ts
+++ mod.ts
@@ -1,67 +1,91 @@
-import { delimiter, join } from "https://deno.land/std@0.113.0/path/mod.ts";
+import { delimiter, join } from 'https://deno.land/std@0.113.0/path/mod.ts'
 
+/**
+ * latrex options
+ */
 interface Options {
-  command?: string;
-  args?: string[];
-  inputs?: string[];
-  passes?: number;
-  errorLogsPath?: string;
+  /**
+   * The command to run for your compiling your LaTeX document (pdflatex, xetex, /user/bin/custom-tex, etc).
+   * Defaults to `pdflatex`.
+   */
+  command?: string
+
+  /**
+   * Arguments passed to the command. Defaults to ['-halt-on-error'].
+   */
+  args?: string[]
+
+  /**
+   * A list of absolute paths to the directory which contains the assets necessary for the doc.
+   */
+  inputs?: string[]
+
+  /**
+   * The number of times to run options.command. Some documents require multiple
+   * passes. Only works when doc is a string. Defaults to 1.
+   */
+  passes?: number
+
+  /**
+   * The path to the file where you want to save the contents of the error log
+   * to.
+   */
+  errorLogsPath?: string
 }
 
-export async function denotex(
+export async function latrex(
   texDoc: Uint8Array | string,
-  options: Options = {},
+  options: Options = {}
 ): Promise<Uint8Array> {
-  const tempDir = await Deno.makeTempDir();
-  const jobName = "denotex";
+  const tempDir = await Deno.makeTempDir()
+  const jobName = 'latrex'
   const {
-    command = "pdflatex",
+    command = 'pdflatex',
     args = [],
     inputs = [tempDir],
-    passes = 1,
-  } = options;
-  const inputsPath = inputs.join(delimiter) + delimiter;
+    passes = 1
+  } = options
+  const inputsPath = inputs.join(delimiter) + delimiter
 
-  const encodedDocument = texDoc instanceof Uint8Array
-    ? texDoc
-    : new TextEncoder().encode(texDoc);
+  const encodedDocument =
+    texDoc instanceof Uint8Array ? texDoc : new TextEncoder().encode(texDoc)
 
   for (let i = 0; i < passes; i++) {
     const process = Deno.run({
-      cmd: [command, `-jobname=${jobName}`, "-halt-on-error", ...args],
+      cmd: [command, `-jobname=${jobName}`, '-halt-on-error', ...args],
       cwd: tempDir,
       env: {
         TEXINPUTS: inputsPath,
         TTFONTS: inputsPath,
-        OPENTYPEFONTS: inputsPath,
+        OPENTYPEFONTS: inputsPath
       },
-      stdin: "piped",
-      stdout: "piped",
-    });
+      stdin: 'piped',
+      stdout: 'piped'
+    })
 
-    await process.stdin.write(encodedDocument);
-    process.stdin.close();
+    await process.stdin.write(encodedDocument)
+    process.stdin.close()
 
-    const status = await process.status();
+    const status = await process.status()
     if (!status.success) {
-      let errorMessage = "denotex was unable to compile the LaTeX document.";
+      let errorMessage = 'latrex was unable to compile the LaTeX document.'
 
       if (options.errorLogsPath) {
-        const logPath = join(tempDir, `${jobName}.log`);
-        await Deno.copyFile(logPath, options.errorLogsPath);
+        const logPath = join(tempDir, `${jobName}.log`)
+        await Deno.copyFile(logPath, options.errorLogsPath)
       } else {
         errorMessage +=
-          "You can pass the `errorLogsPath` flag to see the full error output.";
+          'You can pass the `errorLogsPath` option to `latrex()` to see the full error output.'
       }
 
-      await Deno.remove(tempDir, { recursive: true });
-      throw new Error(errorMessage);
+      await Deno.remove(tempDir, { recursive: true })
+      throw new Error(errorMessage)
     }
   }
 
-  const outputPath = join(tempDir, `${jobName}.pdf`);
-  const pdf = await Deno.readFile(outputPath);
-  await Deno.remove(tempDir, { recursive: true });
+  const outputPath = join(tempDir, `${jobName}.pdf`)
+  const pdf = await Deno.readFile(outputPath)
+  await Deno.remove(tempDir, { recursive: true })
 
-  return pdf;
+  return pdf
 }
